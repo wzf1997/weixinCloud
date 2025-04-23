570fe3596f5464fa50c5b98fab9accf9aa4443ed
wangzhenfei <wangzhengfei@shizhuang-inc.com>
2025-04-23 21:11:10 +0800

    feat: 云开发代码提交

---
+++ index.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { init: initDB, Counter } = require("./db");
+
+const logger = morgan("tiny");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+// 配置微信参数
+const WECHAT_CONFIG = {
+  appid: "wx9cac1c0a47db87e9", // 建议使用环境变量
+  secret: "5d2d7e118bbde6aed3d13cc167eac672",
+};
+
+// 首页
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 更新计数
+app.post("/api/count", async (req, res) => {
+  const { action } = req.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
+    await Counter.destroy({
+      truncate: true,
+    });
+  }
+  res.send({
+    code: 0,
+    data: await Counter.count(),
+  });
+});
+
+// 获取计数
+app.get("/api/count", async (req, res) => {
+  const result = await Counter.count();
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.send(req.headers["x-wx-openid"]);
+  }
+});
+
+// 获取微信access_token
+app.get("/api/wx/token", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.weixin.qq.com/cgi-bin/token",
+      {
+        params: {
+          grant_type: "client_credential",
+          appid: WECHAT_CONFIG.appid,
+          secret: WECHAT_CONFIG.secret,
+        },
+      }
+    );
+
+    res.send({
+      code: 200,
+      data: response.data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      code: -1,
+      error: error.message,
+    });
+  }
+});
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+  await initDB();
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+
+bootstrap();
