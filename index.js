59c8bd650be8dfd74586ff7b1b7f53c68554723b
wangzhenfei <wangzhengfei@shizhuang-inc.com>
2025-04-28 17:08:16 +0800

    feat: 修改json限制

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const logger = morgan("tiny");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.use(logger);
 
